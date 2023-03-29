2557bcd5cbaa696d77cec5ec0f41211913e5ec46
Alberto Ceballos <alcebal2002@yahoo.es>
2023-03-29 09:49:48 +0200

    Removed replacePlaceholders until 1.4.0 is available

--- src/test/java/karate-config-demo.js
+++ src/test/java/karate-config-demo.js
@@ -2,9 +2,7 @@ function fn() {
     karate.log('executing karate-config-demo...');
     karate.log('karate.env system property set to ' + karate.env);
     const configYaml = karate.read('classpath:application-demo.yml');
-
-    let replacePlaceholders = karate.read('classpath:js/replace.js');
-
+    let replacePlaceholdersFunction = karate.read('classpath:js/replace.js');
     let config = {
         database: {
             jdbcUrl: configYaml.application.datasource.jdbcUrl
@@ -13,16 +11,18 @@ function fn() {
             yamlConfiguration: configYaml
         },
         functions: {
-            replacePlaceholders: replacePlaceholders
+            replacePlaceholders: replacePlaceholdersFunction
         }
     };
 
     // karate.log('config before calling javascript with String: INSERT ${services.database.port}');
-    let result = replacePlaceholders('INSERT ${services.database.port}', configYaml);
     // karate.log('config after calling javascript with String: ' + result);
-
     // karate.log('config before calling javascript: ' + (JSON.stringify(config)));
-    config = replacePlaceholders(config, configYaml);
+    // karate.log('before config: ' + config);
+    // karate.log('before config stringify: ' + JSON.stringify(config));
+    // config = replacePlaceholdersFunction(config, configYaml);
+    // karate.log('config: ' + config);
+    // karate.log('config stringify: ' + JSON.stringify(config));
     // karate.log('javascript result type is ' + (typeof config));
     // karate.log('config after calling javascript: ' + config);
     // karate.log('config after calling javascript: ' + (JSON.stringify(config)));
